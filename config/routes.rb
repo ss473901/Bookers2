9b6243453aa833cce8dc2ad6e811a528f1cd1236
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-20 06:29:17 +0000

    トップページのURLを　/　

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
   get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  root to: 'homes#top'
   get 'about' => 'homes#about'
+  
+  resources :books
+ 
 
 end
