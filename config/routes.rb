8cda477e6d243db90b3d3dd5d50d56fc6b7389cb
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-20 12:32:19 +0000

    Add top.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  
+  post 'books' => 'books#create'
   resources :books
  
 
