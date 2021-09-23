466e6b3f27ba3e57ec37b0c66b64dc9be51e22d3
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-23 03:50:36 +0000

    ログイン　メール⇨名前に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
   def after_sign_in_path_for(resource)
    user_path(resource)
   end
  
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  
+ def configure_permitted_parameters
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
