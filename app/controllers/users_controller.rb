a7bdb23a9f9ff3ab71b6ff604695b9d184c26799
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-24 09:26:55 +0000

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,32 @@
 class UsersController < ApplicationController
+   
+  def index
+   @users = User.all
+  end
   
- def index
-  @users = User.all
- end
- 
- def create
-  @book = Book.new(book_params)
-  @book.user_id = current_user.id
-  @book.save
-  redirect_to book_path(@book.id)
- end
+  def create
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   @book.save
+   redirect_to user_path(@book.id)
+  end
+   
+  def show
+   @user = User.find(params[:id])
+  end
+   
+  def edit
+   @user = User.find(params[:id])
+  end
+   
+  def update
+   @user = User.find(params[:id])
+   @user.update(user_params)
+   redirect_to user_path(@user.id)
+  end
   
- def show
-  @user = User.find(params[:id])
+  private
+  def user_params
+   params. require(:user) .permit(:name, :email, :profile_image, :profile)
+  end
  end
-  
- def edit
-  @user = User.find(params[:id])
- end
-  
- def update
-  @user = User.find(params[:id])
-  @user.update(user_params)
-  redirect_to user_path(@user.id)
- end
- 
- private
- def user_params
-  params. require(:user) .permit(:name, :email, :profile_image, :profile)
- end
-end
