f52fb7f59e623ce9811c630d8318c048f19ec615
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-25 06:22:24 +0000

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,12 @@ class UsersController < ApplicationController
    @book = Book.new(book_params)
    @book.user_id = current_user.id
    @book.save
-   redirect_to user_path(@book.id)
+   redirect_to books_path(@book.id)
   end
-   
+  
   def show
    @user = User.find(params[:id])
+   @book = Book.new
   end
    
   def edit
