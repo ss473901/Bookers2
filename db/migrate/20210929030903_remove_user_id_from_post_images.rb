b7128589bde44739d2e9698b82843e43b126dae5
ss473901 <satoshi.473901.1205@gmail.com>
2021-09-29 04:30:11 +0000

    Add

--- db/migrate/20210929030903_remove_user_id_from_post_images.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromPostImages < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :post_images, :user_id, :integer
-    drop_table :post_images
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_030903) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
