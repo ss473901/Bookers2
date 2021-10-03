7fde04d261af193e35eb9f131c1b2eb9b3e63143
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-03 08:20:41 +0000

    FiX

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def create
-    following = current_user.relationships.build(follower_id: :params[:user_id])
+    following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
     redirect_to request.referrer || root_path
   end
 
   def destroy
-   following = current_user.relationships.find_by(follower_id: :params[:user_id])
+   following = current_user.relationships.find_by(follower_id: params[:user_id])
    following.save
    redirect_to request.referrer || root_path
   end
