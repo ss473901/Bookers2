56cef02df08d5eac066eaa27aff7b8fbc1824f0c
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-05 11:06:51 +0000

    Fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,35 +1,37 @@
 class SearchsController < ApplicationController
- def serch
- # viewのform_tagにて
-    # 選択したmodelの値を@modelに代入。
-    @model = params["model"]
-    # 選択した検索方法の値を@methodに代入。
-    @method = params["method"]
-    # 検索ワードを@contentに代入。
-    @content = params["content"]
-    # @model, @content, @methodを代入した、
-    # search_forを@recordsに代入。
-    @records = search_for(@model, @content, @method)
+def search_for
+  @model = params["model"]
+  @content = params["content"]
+  @method = params["method"]
+  @records = search_for(@model. @content, @method)
 end
-    
- def search_for(model, content, method)
-    # 選択したモデルがuserだったら
-    if model == 'user'
-      # 選択した検索方法がが完全一致だったら
-      if method == 'perfect'
-        User.where(name: content)
-      # 選択した検索方法がが部分一致だったら
-      else
-        User.where('name LIKE ?', '%'+content+'%')
-      end
-    # 選択したモデルがbookだったら
-    elsif model == 'book'
-      if method == 'perfect'
-        Book.where(title: content)
-      else
-        Book.where('title LIKE ?', '%'+content+'%')
-      end
-    end
+
+privata
+def search_for(model,content,method)
+  if model == 'user'
+  if method == "forward_match"
+    User.where("name LIKE?", "#{content}%")
+  elsif method == "backward_match"
+    User.where("name LIKE?", "#{content}%")
+  elsif method == "perfect_match"
+    User.where("#{content}")
+  elsif method == "partial_match"
+    User.where("name LIKE?", "%#{content}%")
+  else
+    User.all
   end
+  elsif model == 'book'
+  if method == "forward_match"
+    Book.where("name LIKE?", "#{content}%")
+  elsif method == "backward_match"
+    Book.where("name LIKE?", "#{content}%")
+  elsif method == "perfect_match"
+    Book.where("#{content}")
+  elsif method == "partial_match"
+    Book.where("name LIKE?", "%#{content}%")
+  else
+    Book.all
+  end
+end
 end
 
