c59f892f2a55d061c5c2eea7f3a899c26c0313b7
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-05 11:28:17 +0000

    Fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,37 +1,43 @@
-class SearchsController < ApplicationController
-def search_for
-  @model = params["model"]
-  @content = params["content"]
-  @method = params["method"]
-  @records = search_for(@model. @content, @method)
-end
+class SearchesController < ApplicationController
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
 
-privata
-def search_for(model,content,method)
-  if model == 'user'
-  if method == "forward_match"
-    User.where("name LIKE?", "#{content}%")
-  elsif method == "backward_match"
-    User.where("name LIKE?", "#{content}%")
-  elsif method == "perfect_match"
-    User.where("#{content}")
-  elsif method == "partial_match"
-    User.where("name LIKE?", "%#{content}%")
-  else
-    User.all
+private
+  def search_for(model,content,method)
+    if model == 'user'
+
+      if method == "forward_match"
+        User.where("name LIKE?", "#{content}%")
+      elsif method == "backward_match"
+        User.where("name LIKE?", "#{content}%")
+      elsif method == "perfect_match"
+        User.where("#{content}")
+      elsif method == "partial_match"
+        User.where("name LIKE?", "%#{content}%")
+      else
+        User.all
+      end
+
+    elsif model == 'book'
+
+      if method == "forward_match"
+        Book.where("name LIKE?", "#{content}%")
+      elsif method == "backward_match"
+        Book.where("name LIKE?", "#{content}%")
+      elsif method == "perfect_match"
+        Book.where("#{content}")
+      elsif method == "partial_match"
+        Book.where("name LIKE?", "%#{content}%")
+      else
+        Book.all
+      end
+
+    end
+
   end
-  elsif model == 'book'
-  if method == "forward_match"
-    Book.where("name LIKE?", "#{content}%")
-  elsif method == "backward_match"
-    Book.where("name LIKE?", "#{content}%")
-  elsif method == "perfect_match"
-    Book.where("#{content}")
-  elsif method == "partial_match"
-    Book.where("name LIKE?", "%#{content}%")
-  else
-    Book.all
-  end
-end
 end
 
